9b623b0ac4a10de63c69de0b30ceb7e83d6234ce
Bertolami <roman@bertolami.net>
2009-11-22 15:29:51 +0100

    chapter 6

---
+++ ProgrammingInScala/src/programminginscala/chapter6/Chapter6.scala
@@ -0,0 +1,20 @@
+/*
+ * Chapter6.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package programminginscala.chapter6
+
+object Chapter6 {
+
+  /**
+   * @param args the command line arguments
+   */
+  def main(args: Array[String]) :Unit = {
+    val r = new Rational(3,4)
+    println(r)
+  }
+
+}
---
+++ ProgrammingInScala/src/programminginscala/chapter6/Rational.scala
@@ -0,0 +1,12 @@
+/*
+ * Rational.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package programminginscala.chapter6
+
+class Rational(n: Int, d: Int) {
+    override def toString =  n +"/"+d
+}
