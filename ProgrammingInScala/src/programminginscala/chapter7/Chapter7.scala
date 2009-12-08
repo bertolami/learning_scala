edbe1e1b4c17c6a33bb870f0bd00ec84b222107f
Bertolami <roman@bertolami.net>
2009-12-08 09:09:26 +0100

    chapter 7

---
+++ ProgrammingInScala/src/programminginscala/chapter7/Chapter7.scala
@@ -0,0 +1,79 @@
+/*
+ * Chapter7.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package programminginscala.chapter7
+
+object Chapter7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    def main(args: Array[String]) :Unit = {
+        println(gcdLoop(12, 8))
+        println(gcdLoop(8, 12))
+
+        println(gcd(12, 8))
+        println(gcd(8, 12))
+
+        simpleForLoop
+
+        fileForLoop
+
+        grep(".*gcd.*")
+    }
+
+    def scalaFiles = {
+        for {file <- filesHere
+            if(file.getName.endsWith(".scala"))
+        } yield file
+    }
+    
+    def grep(pattern: String) = {
+        for {file <- scalaFiles
+             line <- fileLines(file)
+             trimmed = line.trim
+             if(trimmed.matches(pattern))} {
+            println(file +": "+ line.trim)
+        }
+    }
+    
+    def fileLines(file: java.io.File) = scala.io.Source.fromFile(file).getLines.toList
+
+    def fileForLoop {
+        for (file <- filesHere
+             if file.isFile;
+             if file.getName.endsWith(".scala")) {
+            println(file)
+        }
+
+    }
+
+   
+
+    def filesHere = (new java.io.File("src/programminginscala/chapter7")).listFiles
+
+    def simpleForLoop {
+        for (i <- 1 to 4)
+        println("Iteration "+i)
+        for (i <- 1 until 4)
+        println("Iteration "+i)
+    }
+
+    def gcdLoop(x: Long, y: Long) : Long = {
+        var a = x
+        var b = y
+        while (a != 0) {
+            val temp = a
+            a = b % a
+            b = temp
+        }
+        b
+    }
+
+    def gcd(x: Long, y: Long) : Long = if(y == 0) x else gcd(y, x % y)
+
+}
