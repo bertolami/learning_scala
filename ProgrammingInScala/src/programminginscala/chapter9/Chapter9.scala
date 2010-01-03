210f0d0259771a33b8527ff85f186b5635ea0b2d
Bertolami <roman@bertolami.net>
2010-01-03 16:40:59 +0100

    chapter 9

---
+++ ProgrammingInScala/src/programminginscala/chapter9/Chapter9.scala
@@ -0,0 +1,19 @@
+/*
+ * Chapter9.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package programminginscala.chapter9
+
+object Chapter9 {
+
+  /**
+   * @param args the command line arguments
+   */
+  def main(args: Array[String]) :Unit = {
+    FileMatcher.filesEnding("scala")
+  }
+
+}
---
+++ ProgrammingInScala/src/programminginscala/chapter9/FileMatcher.scala
@@ -0,0 +1,12 @@
+/*
+ * FileMatcher.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package programminginscala.chapter9
+
+object FileMatcher {
+
+}
