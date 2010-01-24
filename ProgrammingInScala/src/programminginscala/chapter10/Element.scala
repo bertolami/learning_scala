27a020d238fa82c9fad615480ed561aafe1d20f0
Bertolami <roman@bertolami.net>
2010-01-24 17:04:38 +0100

    chapter 10

--- ProgrammingInScala/src/programminginscala/chapter10/Element.scala
+++ ProgrammingInScala/src/programminginscala/chapter10/Element.scala
@@ -6,9 +6,33 @@
  */
 
 package programminginscala.chapter10
+import Element.elem
+
+object Element {
+    def elem(contents: Array[String]) : Element =
+        new ArrayElement(contents)
+    def elem(chr: Char, width: Int, height: Int) : Element =
+        new UniformElement(chr, width, height)
+    def elem(line: String):Element =
+        new LineElement(line)
+}
 
 abstract class Element {
     def contents: Array[String]
+
     def height = contents.length
+
     def width = if(height == 0) 0 else contents(0).length
-}
+    
+    def above(that: Element) : Element = elem(this.contents ++ that.contents)
+
+    def beside(that: Element): Element = {
+        elem(
+            for((line1, line2) <- this.contents zip that.contents)
+                yield line1 + line2
+        )
+    }
+
+    override def toString = contents mkString "\n"
+    
+ }
--- ProgrammingInScala/src/programminginscala/chapter10/LineElement.scala
+++ ProgrammingInScala/src/programminginscala/chapter10/LineElement.scala
@@ -7,7 +7,8 @@
 
 package programminginscala.chapter10
 
-class LineElement(s: String) extends ArrayElement(Array(s)) {
+class LineElement(s: String) extends Element  {
+    val contents = Array(s)
     override def width = s.length
     override def height = 1
 }
